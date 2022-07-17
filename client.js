a464f7efc90bfa5b038d6e5b9b74769ba2433192
Latisha Copeland <lcopeland001@gmail.com>
2022-07-16 22:52:27 -0500

    collect ino

--- client.js
+++ client.js
@@ -4,9 +4,21 @@ $(document).ready(readyNow);
 
 function readyNow(){
  console.log('ready now');
+ $('#submitButton').on('click', submitButton);
 } // end readyNow
+
+let emplyeeInfo = [];
 
 function submitButton(){
     console.log('in submitButton');
+    let employee = {
+        firstName: $('#firstNameInput').val(),
+        lastName:$('#lastNameInput').val(),
+        id: $('#idInput').val(),
+        title: $('#titleInput').val(),
+        annualSalary: $('#annualSalaryInput').val(),
+    };
+    emplyeeInfo.push(employee);
 } // end submitButton
+
 
